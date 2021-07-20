4f9b4c345f627fc5b14e61b637cc2da8f1f9d74f
kha-github <rlahyuna1234@gmail.com>
2021-07-20 15:01:11 +0900

    spring project init

---
+++ src/main/java/team/kelly/kellyserver/KellyserverApplication.java
@@ -0,0 +1,13 @@
+package team.kelly.kellyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KellyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KellyserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/kelly/kellyserver/KellyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package team.kelly.kellyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KellyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
