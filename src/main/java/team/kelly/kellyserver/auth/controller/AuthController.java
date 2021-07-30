0cc24c85509403cc15ed38b902b7080f9a0beabf
kha-github <rlahyuna1234@gmail.com>
2021-07-30 21:18:07 +0900

    [TKLY-98] swagger 적용

--- src/main/java/team/kelly/kellyserver/auth/controller/AuthController.java
+++ src/main/java/team/kelly/kellyserver/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package team.kelly.kellyserver.auth.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -37,11 +38,13 @@ public class AuthController {
 
 
     @PostMapping("/authenticate")
+    @ApiOperation(value="엑세스 토큰 & 리프레시 토큰 발급", notes="유저 데이터로 엑세스 토큰과 리프레시 토큰을 발급한다.")
     public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
         return ResponseEntity.ok(authService.authorize(loginDto));
     }
 
     @PostMapping("/reissue")
+    @ApiOperation(value="엑세스 토큰 & 리프레시 토큰 재발급", notes="엑세스 토큰과 리프레시 토큰으로 새로운 엑세스 토큰과 리프레시 토큰을 발급한다.")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
         Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
 
