a9c2d16c26a8d9b121238bdc84458dff6250418f
kha-github <rlahyuna1234@gmail.com>
2022-01-06 01:57:02 +0900

    [TKLY-160] 길찾기 mock api 추가

--- src/main/java/team/kelly/kellyserver/mock/MockApiController.java
+++ src/main/java/team/kelly/kellyserver/mock/MockApiController.java
@@ -2,10 +2,7 @@ package team.kelly.kellyserver.mock;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.kelly.kellyserver.mock.dto.MockRouteDto;
 import team.kelly.kellyserver.mock.dto.MockRouteSearchDto;
 import team.kelly.kellyserver.mock.dto.MockWelcomPhraseDto;
@@ -25,7 +22,7 @@ public class MockApiController {
     }
 
     @PostMapping("/routeList")
-    public List<MockRouteDto> getRouteList(MockRouteSearchDto mockRouteSearchDto) {
+    public List<MockRouteDto> getRouteList(@RequestBody MockRouteSearchDto mockRouteSearchDto) {
         log.info(mockRouteSearchDto.toString());
         List<MockRouteDto> mockRouteDtoList = new ArrayList<>();
         mockRouteDtoList.add(RouteDataGenerator.get1());
